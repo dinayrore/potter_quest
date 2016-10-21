4b69aedb2babfd7c77d981a2cac743ea65fe5cb9
donaldsonjulia <julia.donaldson@gmail.com>
2016-10-21 16:03:43 -0400

    linking profile to user info database

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'users#new' # login/signup
 
-  get  '/profile', to: 'pages#profile' # user profile (static)
   get  '/leaderboard', to: 'pages#leaderboard' # leaderboard (static)
   get  '/game', to: 'pages#game' # game page (static)
+
 
   post '/signup', to: 'users#create' # creates a user
 
