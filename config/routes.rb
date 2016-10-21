0af32d019705c7b9262f07216869b6d3e5a1c32f
Kristine <kteich88@gmail.com>
2016-10-21 16:05:10 -0400

    attempting file path changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'users#new' # login/signup
 
-  get  '/profile', to: 'pages#profile' # user profile (static)
+  get  '/profile', to: 'user#show' # user profile (static)
   get  '/leaderboard', to: 'pages#leaderboard' # leaderboard (static)
   get  '/game', to: 'pages#game' # game page (static)
 
