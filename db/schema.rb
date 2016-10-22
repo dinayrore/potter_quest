e5396e1a3f5d7dd5fcf608a1863276e90cdf20a7
Kristine <kteich88@gmail.com>
2016-10-22 19:57:10 -0400

    recreated file migrations for clarity. made user id foreign key of games.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022184433) do
+ActiveRecord::Schema.define(version: 5) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,4 +40,5 @@ ActiveRecord::Schema.define(version: 20161022184433) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "games", "users"
 end
