2f16a4a2801dc9fc107b7e4b3538d1544145982b
Kristine <kteich88@gmail.com>
2016-10-21 20:57:11 -0400

    still attempting routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
 
   get  '/leaderboard', to: 'pages#leaderboard' # leaderboard (static)
   get  '/game', to: 'pages#game' # game page (static)
-  get '/users/:id', to: 'users#profile' # should lead to user specific page
+  get '/users/:id', to: 'users#show' # should lead to user specific page
+
   post '/signup', to: 'users#create' # creates a user
 
-  get    '/login',   to: 'sessions#new' # login page
+  get    '/login',   to: 'sessions#new' # login page for a specific user
   post   '/login',   to: 'sessions#create' # create new session
   delete '/logout',  to: 'sessions#destroy' # destroy session
 
