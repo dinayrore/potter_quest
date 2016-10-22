e2536557a273321530b55b8f6734317e85dbd1e0
donaldsonjulia <julia.donaldson@gmail.com>
2016-10-22 15:00:03 -0400

    data base created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161022151114) do
+ActiveRecord::Schema.define(version: 20161022184433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "scene"
+    t.string   "inventory"
+    t.integer  "score"
+    t.integer  "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["inventory"], name: "index_games_on_inventory", using: :btree
+    t.index ["scene"], name: "index_games_on_scene", using: :btree
+    t.index ["score"], name: "index_games_on_score", using: :btree
+    t.index ["time"], name: "index_games_on_time", using: :btree
+    t.index ["user_id"], name: "index_games_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
