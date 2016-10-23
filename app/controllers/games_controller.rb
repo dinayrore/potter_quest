ab05a72ef8674a63d09c9620f99ac09e3e60b0fa
Robby Helms <robert.helms1@gmail.com>
2016-10-22 23:40:52 -0400

    Added ability to save games.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,6 +11,7 @@ class GamesController < ApplicationController
   def create
     @game = Game.new(game_params)
     if @game.save
+      save_data @game
       redirect_to @game
     else
       render 'new'
